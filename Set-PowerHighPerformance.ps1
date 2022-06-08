0a428eb8aae42a2279549551c9606e46b256c26a
Dan Meddock <dmeddock@kandeconsulting.com>
2022-06-08 14:11:14 -0500

    PPKG Updates

--- Set-PowerHighPerformance.ps1
+++ Set-PowerHighPerformance.ps1
@@ -10,7 +10,14 @@ LAST UPDATED: 29MAR2022
 Start-Transcript -Path "C:\temp\PPKG-setHighPreformance.log"  
 
 # Main
-Try{	
+Try{
+	# Test if the device has internet connection; if not then fail and end PPKG setup process.
+	if (!(test-connection 8.8.8.8 -Count 1 -Quiet)){
+    write-host "No Internet"
+	Exit 1
+	}else{write-host "Device is Online."}
+	
+	# Set High Preformance power scheme
 	Write-Host "Applying High Preformance power settings..."
 	powercfg.exe /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 }Catch{
@@ -18,3 +25,4 @@ Try{
 }
 
 Stop-Transcript
+
